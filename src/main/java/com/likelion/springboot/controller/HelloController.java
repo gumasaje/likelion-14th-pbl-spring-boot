f1e707c5f4cd081a227c14bdb7a4941591587e24
gumasaje <191728697+gumasaje@users.noreply.github.com>
2026-05-14 18:54:45 +0900

    [6주차] feat: 이름을 받아 인사하는 API (Bonus 1)
    - PathVariable 어노테이션을 사용하여 URL 경로에서 이름을 추출하는 기능 구현
    - GET /hello/{name} 요청 시 해당 이름이 포함된 인사말 반환

--- src/main/java/com/likelion/springboot/controller/HelloController.java
+++ src/main/java/com/likelion/springboot/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.likelion.springboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello, Likelion!";
+    }
+
+    // Bonus 1
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable("name") String name) {
+        return "Hello, " + name + "!";
     }
 }
