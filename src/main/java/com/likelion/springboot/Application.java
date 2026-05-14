95fa7675bd73d880af8f8871c8a6c8dd8eb7821f
gumasaje <191728697+gumasaje@users.noreply.github.com>
2026-05-14 18:32:40 +0900

    [6주차] feat: 수동 주입으로 Bean 등록 (Step 2)
    - Configuration 어노테이션을 사용한 AppConfig 클래스 생성
    - Bean 어노테이션을 통해 MemoryMemberRepository 및 MemberService 수동 등록
    - ApplicationContext를 통해 등록된 빈의 정상 로드 확인
    - 5주차의 수동 객체 조립 방식을 스프링 IoC 컨테이너 관리 방식으로 전환

--- src/main/java/com/likelion/springboot/Application.java
+++ src/main/java/com/likelion/springboot/Application.java
@@ -1,13 +1,17 @@
 package com.likelion.springboot;
 
+import com.likelion.springboot.service.MemberService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+        ApplicationContext context = SpringApplication.run(Application.class, args);
 
+        MemberService memberService = context.getBean(MemberService.class);
+        System.out.println("서비스 가져오기 성공: " + memberService);
+
+    }
 }
