1c07a70288b747fe8ee88e4bcae111ba5447b1ff
gumasaje <191728697+gumasaje@users.noreply.github.com>
2026-05-15 14:09:23 +0900

    [7주차] feat: MemberService에 CRUD 메서드 추가 (Step 2)
    - Lion/Staff 생성을 위한 createLion, createStaff 메서드 구현
    - 새로운 객체 생성 및 Repository 교체 방식의 업데이트 로직 구현
    - 이름 기반 멤버 삭제를 위한 deleteMember 메서드 구현
    - 비즈니스 로직에 따른 예외 처리 적용

--- src/main/java/com/likelion/springboot/service/MemberService.java
+++ src/main/java/com/likelion/springboot/service/MemberService.java
@@ -1,5 +1,11 @@
 package com.likelion.springboot.service;
 
+import com.likelion.springboot.domain.role.Lion;
+import com.likelion.springboot.domain.role.Staff;
+import com.likelion.springboot.dto.LionCreateRequest;
+import com.likelion.springboot.dto.LionUpdateRequest;
+import com.likelion.springboot.dto.StaffCreateRequest;
+import com.likelion.springboot.dto.StaffUpdateRequest;
 import com.likelion.springboot.repository.MemberRepository;
 import com.likelion.springboot.domain.role.Role;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +22,77 @@ public class MemberService {
         this.repository = repository;
     }
 
-    public boolean register(Role member) {
-        if (repository.existsByName(member.getName())) {
-            return false;
+    public Role createLion(LionCreateRequest lionCreateRequest) {
+        Lion lion = new Lion(
+                lionCreateRequest.getName(),
+                lionCreateRequest.getMajor(),
+                lionCreateRequest.getGeneration(),
+                lionCreateRequest.getPart(),
+                lionCreateRequest.getStudentId()
+        );
+
+        if (repository.existsByName(lion.getName())) {
+            return null;
         }
-        repository.save(member);
-        return true;
+
+        repository.save(lion);
+        return lion;
+
+    }
+
+    public Role createStaff(StaffCreateRequest staffCreateRequest) {
+        Staff staff = new Staff(
+                staffCreateRequest.getName(),
+                staffCreateRequest.getMajor(),
+                staffCreateRequest.getGeneration(),
+                staffCreateRequest.getPart(),
+                staffCreateRequest.getPosition()
+        );
+
+        if (repository.existsByName(staff.getName())) {
+            return null;
+        }
+
+        repository.save(staff);
+        return staff;
+    }
+
+    public Role updateLion(String name, LionUpdateRequest lionUpdateRequest) {
+        if (repository.findByName(name) == null) {
+            return null;
+        }
+
+        Lion lion = new Lion(
+                name,
+                lionUpdateRequest.getMajor(),
+                lionUpdateRequest.getGeneration(),
+                lionUpdateRequest.getPart(),
+                lionUpdateRequest.getStudentId()
+        );
+
+        repository.updateByName(name, lion);
+        return lion;
+    }
+
+    public Role updateStaff(String name, StaffUpdateRequest staffUpdateRequest) {
+        if (repository.findByName(name) == null) {
+            return null;
+        }
+
+        Staff staff = new Staff(
+                name,
+                staffUpdateRequest.getMajor(),
+                staffUpdateRequest.getGeneration(),
+                staffUpdateRequest.getPart(),
+                staffUpdateRequest.getPosition()
+        );
+
+        repository.updateByName(name, staff);
+        return staff;
+    }
+
+    public boolean deleteMember(String name) {
+        return repository.deleteByName(name);
     }
 
     public Role searchByName(String name) {
