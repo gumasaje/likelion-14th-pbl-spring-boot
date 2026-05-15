0a2867c5cc6cf32da9e556386622bc7363673d04
gumasaje <191728697+gumasaje@users.noreply.github.com>
2026-05-15 14:09:23 +0900

    [7주차] feat: MemberController 생성 (Step 4)
    - 멤버 관련 API 처리를 위한 MemberController 클래스 생성
    - RestController 및 RequestMapping 어노테이션 적용
    - MemberService에 대한 생성자 기반 자동 의존성 주입 구현

---
+++ src/main/java/com/likelion/springboot/controller/MemberController.java
@@ -0,0 +1,16 @@
+package com.likelion.springboot.controller;
+
+import com.likelion.springboot.service.MemberService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
